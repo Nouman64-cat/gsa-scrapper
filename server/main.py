50c05cb7930b6ab56aee179221909480effc7a10
Nouman64-cat <noumanejazghost@gmail.com>
2026-04-24 16:17:40 -0400

    chore: update server port to 9000 and client dev port to 4000

--- server/main.py
+++ server/main.py
@@ -67,4 +67,4 @@ app.include_router(status.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
